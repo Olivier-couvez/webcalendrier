65b51f3b6063a0c5ade343b12ea382850c045df8
Couvez olivier <o.couvez@free.fr>
2021-10-01 09:16:34 +0200

    Mise en place web calendrier en français.

---
+++ includes/settings.php
@@ -0,0 +1,17 @@
+<?php
+/* updated via install/index.php on Tue, 28 Sep 2021 09:41:06 -0400
+install_password: a23b2f64e980982dcb0e11ff1a84c828
+db_cachedir: c:\temp
+db_database: intranet
+db_host: localhost
+db_login: olivier
+db_persistent: true
+db_type: mysqli
+readonly: false
+single_user: false
+use_http_auth: false
+user_inc: user.php
+db_password: olivier
+mode: prod
+# end settings.php */
+?>
